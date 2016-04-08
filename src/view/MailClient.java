254a08cd422e172cc68bcb3f5150d8e895f81b7d
Jonas Segessemann <segessemann@mst.local>
2016-04-08 19:10:43 +0200

    Hinzufügen von Toolbox Buttons Erstellen von Account Settings Dialog
    Signed-off-by: Jonas Segessemann <segessemann@mst.local>

--- src/view/MailClient.java
+++ src/view/MailClient.java
@@ -35,54 +35,134 @@ public class MailClient extends javax.swing.JFrame {
 
         jPanelHeader = new javax.swing.JPanel();
         jToolBarHeader = new javax.swing.JToolBar();
+        jButtonNewFolder = new javax.swing.JButton();
+        jButtonDeleteFolder = new javax.swing.JButton();
+        jButtonNewMail = new javax.swing.JButton();
+        jButtonDeleteMail = new javax.swing.JButton();
         jSplitPane = new javax.swing.JSplitPane();
         jScrollPaneMailTree = new javax.swing.JScrollPane();
         jMailTree = new javax.swing.JTree();
         jScrollPaneMailTable = new javax.swing.JScrollPane();
         jMailTable = new javax.swing.JTable();
         jMenuBar = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        jMenuFile = new javax.swing.JMenu();
+        jMenuItemAccSettings = new javax.swing.JMenuItem();
+        jMenuItemExit = new javax.swing.JMenuItem();
+        jMenuSettings = new javax.swing.JMenu();
+        jMenuItemVersion = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jToolBarHeader.setRollover(true);
+        jToolBarHeader.setBorderPainted(false);
+
+        jButtonNewFolder.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/img/addfolder.png"))); // NOI18N
+        jButtonNewFolder.setText("add Folder");
+        jButtonNewFolder.setFocusable(false);
+        jButtonNewFolder.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButtonNewFolder.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButtonNewFolder.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonNewFolderActionPerformed(evt);
+            }
+        });
+        jToolBarHeader.add(jButtonNewFolder);
+
+        jButtonDeleteFolder.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/img/delfolder.png"))); // NOI18N
+        jButtonDeleteFolder.setText("delete Folder");
+        jButtonDeleteFolder.setFocusable(false);
+        jButtonDeleteFolder.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButtonDeleteFolder.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButtonDeleteFolder.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonDeleteFolderActionPerformed(evt);
+            }
+        });
+        jToolBarHeader.add(jButtonDeleteFolder);
+
+        jButtonNewMail.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/img/create.png"))); // NOI18N
+        jButtonNewMail.setText("new Mail");
+        jButtonNewMail.setFocusable(false);
+        jButtonNewMail.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButtonNewMail.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButtonNewMail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonNewMailActionPerformed(evt);
+            }
+        });
+        jToolBarHeader.add(jButtonNewMail);
+
+        jButtonDeleteMail.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/img/delMail.png"))); // NOI18N
+        jButtonDeleteMail.setText("delete Mail");
+        jButtonDeleteMail.setFocusable(false);
+        jButtonDeleteMail.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButtonDeleteMail.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jToolBarHeader.add(jButtonDeleteMail);
 
         javax.swing.GroupLayout jPanelHeaderLayout = new javax.swing.GroupLayout(jPanelHeader);
         jPanelHeader.setLayout(jPanelHeaderLayout);
         jPanelHeaderLayout.setHorizontalGroup(
             jPanelHeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jToolBarHeader, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanelHeaderLayout.createSequentialGroup()
+                .addComponent(jToolBarHeader, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanelHeaderLayout.setVerticalGroup(
             jPanelHeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jToolBarHeader, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jToolBarHeader, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
+        jMailTree.setDragEnabled(true);
+        jMailTree.setEditable(true);
+        jMailTree.setName(""); // NOI18N
+        jMailTree.setPreferredSize(new java.awt.Dimension(120, 64));
+        jMailTree.setShowsRootHandles(true);
         jScrollPaneMailTree.setViewportView(jMailTree);
 
         jSplitPane.setLeftComponent(jScrollPaneMailTree);
 
         jMailTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Subject", "Sender", "Date"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jMailTable.setPreferredSize(new java.awt.Dimension(30, 64));
         jScrollPaneMailTable.setViewportView(jMailTable);
 
         jSplitPane.setRightComponent(jScrollPaneMailTable);
 
-        jMenu1.setText("File");
-        jMenuBar.add(jMenu1);
+        jMenuFile.setText("File");
 
-        jMenu2.setText("Edit");
-        jMenuBar.add(jMenu2);
+        jMenuItemAccSettings.setText("Account Settings");
+        jMenuItemAccSettings.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemAccSettingsActionPerformed(evt);
+            }
+        });
+        jMenuFile.add(jMenuItemAccSettings);
+
+        jMenuItemExit.setText("Quit");
+        jMenuFile.add(jMenuItemExit);
+
+        jMenuBar.add(jMenuFile);
+
+        jMenuSettings.setText("?");
+
+        jMenuItemVersion.setText("Version");
+        jMenuSettings.add(jMenuItemVersion);
+
+        jMenuBar.add(jMenuSettings);
 
         setJMenuBar(jMenuBar);
 
@@ -98,11 +178,27 @@ public class MailClient extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanelHeader, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSplitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 495, Short.MAX_VALUE))
+                .addComponent(jSplitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 506, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonNewFolderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNewFolderActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonNewFolderActionPerformed
+
+    private void jButtonDeleteFolderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonDeleteFolderActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonDeleteFolderActionPerformed
+
+    private void jButtonNewMailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNewMailActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonNewMailActionPerformed
+
+    private void jMenuItemAccSettingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemAccSettingsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItemAccSettingsActionPerformed
 
     /**
      * @param args the command line arguments
@@ -140,11 +236,18 @@ public class MailClient extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonDeleteFolder;
+    private javax.swing.JButton jButtonDeleteMail;
+    private javax.swing.JButton jButtonNewFolder;
+    private javax.swing.JButton jButtonNewMail;
     private javax.swing.JTable jMailTable;
     private javax.swing.JTree jMailTree;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar;
+    private javax.swing.JMenu jMenuFile;
+    private javax.swing.JMenuItem jMenuItemAccSettings;
+    private javax.swing.JMenuItem jMenuItemExit;
+    private javax.swing.JMenuItem jMenuItemVersion;
+    private javax.swing.JMenu jMenuSettings;
     private javax.swing.JPanel jPanelHeader;
     private javax.swing.JScrollPane jScrollPaneMailTable;
     private javax.swing.JScrollPane jScrollPaneMailTree;
